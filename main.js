525e4105044e2677dfee6d72e56023d8396c7038
Max Polun <mpolun@mdsol.com>
2014-10-07 17:25:59 -0400

    move process control out of chinook module

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-var chinook = require('./lib/chinook')
+#!/usr/bin/env node
+'use strict'
+var chinook = require('./lib/chinook'),
+    migrationFiles = require('migrationFiles')
 
 var config = chinook.config(process.cwd())
 
-chinook.up(chinook.migrations(config), config)
+function quit() {
+  process.exit()
+}
+
+chinook.up(chinook.migrations(config), config).catch(migrationFiles.NoMigrationError, function noMigrationsFound(){
+    console.log('No migrations found. Exiting.')
+    quit()
+  }).catch(function printError(err){
+    console.error(err)
+    process.exit(1)
+  }).finally(quit)
