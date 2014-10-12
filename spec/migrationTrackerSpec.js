5961747a1de3405ae39939753a316d5ee7469fd8
Max Polun <mpolun@mdsol.com>
2014-10-12 19:58:18 -0400

    refactor migrationTracker, take db parameter in constructor

--- spec/migrationTrackerSpec.js
+++ spec/migrationTrackerSpec.js
@@ -4,13 +4,14 @@ var MigrationTracker = require('migrationTracker'),
     databaseConnection = require('databaseConnection'),
     Promise = require('bluebird')
 
+var testTable = 'migrations_test_table_1'
+
 describe('MigrationTracker', function(){
   it('can be created', function(){
-    var tracker = new MigrationTracker({table: 'migrations_complete'})
+    var tracker = new MigrationTracker(jasmine.createSpy('db'), {table: 'migrations_complete'})
     expect(tracker).not.toBeUndefined()
   })
   describe('create', function(){
-    var testTable = 'migrations_test_table_1'
     beforeEach(function(done){
       Promise.using(databaseConnection(testUtil.dbUrl), function(db){
         return db.query('DROP TABLE IF EXISTS ' + testTable + ';')
@@ -18,17 +19,17 @@ describe('MigrationTracker', function(){
     })
       pit('will create a table if none exists', function(){
       return Promise.using(databaseConnection(testUtil.dbUrl), function(db){
-        var tracker = new MigrationTracker({table: testTable})
-        return tracker.create(db).then(function(){
+        var tracker = new MigrationTracker(db, {table: testTable})
+        return tracker.create().then(function(){
           return db.query('SELECT * FROM ' + testTable + ';')
         })
       })
     })
     pit('wont delete existing migrations', function(){
       return Promise.using(databaseConnection(testUtil.dbUrl), function(db){
-        var tracker = new MigrationTracker({table: testTable}), 
+        var tracker = new MigrationTracker(db, {table: testTable}), 
             previousMigrations
-        return tracker.create(db).then(function(){
+        return tracker.create().then(function(){
           return db.query('SELECT table_schema,table_name FROM information_schema.tables WHERE table_schema = \'public\';')
         }).then(function(){
           return db.query('INSERT INTO ' + testTable + ' (migration_version)  VALUES ($1), ($2), ($3);', [1, 2, 3])
@@ -37,7 +38,7 @@ describe('MigrationTracker', function(){
         }).then(function(results){
           previousMigrations = results.rows
         }).then(function(){
-          return tracker.create(db)
+          return tracker.create()
         }).then(function(){
           return db.query('SELECT * FROM ' + testTable + ';')
         }).then(function(results){
@@ -47,47 +48,46 @@ describe('MigrationTracker', function(){
     })
   })
   describe('up', function(){
-    var tracker,
-        testTable = 'migrations_test_table_1'
+    
     beforeEach(function(done){
-      tracker = new MigrationTracker({table: testTable})
       Promise.using(databaseConnection(testUtil.dbUrl), function(db){
+        var tracker = new MigrationTracker(db, {table: testTable})
         return db.query('DROP TABLE IF EXISTS ' + testTable + ';').then(function(){
-          return tracker.create(db)
+          return tracker.create()
         })
       }).finally(done)
     })
     pit('will update the database', function(){
       return Promise.using(databaseConnection(testUtil.dbUrl), function(db){
-          return tracker.up('12345', db).then(function(){
-            return testUtil.migrationIsMarkedComplete('12345', testTable)
-          })
+        var tracker = new MigrationTracker(db, {table: testTable})
+        return tracker.up('12345').then(function(){
+          return testUtil.migrationIsMarkedComplete('12345', testTable)
+        })
       })
     })
   })
   describe('down', function(){
-    var tracker,
-        testTable = 'migrations_test_table_1'
     beforeEach(function(done){
-      tracker = new MigrationTracker({table: testTable})
       Promise.using(databaseConnection(testUtil.dbUrl), function(db){
+        var tracker = new MigrationTracker(db, {table: testTable})
         return db.query('DROP TABLE IF EXISTS ' + testTable + ';').then(function(){
-          return tracker.create(db)
+          return tracker.create()
         })
       }).finally(done)
     })
     pit('will update the database', function(){
       return Promise.using(databaseConnection(testUtil.dbUrl), function(db){
-          return tracker.up('12345', db).then(function(){
-          }).then(function(){
-            return tracker.down('12345', db)
-          }).then(function(){
-            return testUtil.migrationIsMarkedComplete('12345', testTable)
-          }).then(function(){
-            throw 'migration should not be marked complete, but was'
-          }).catch(testUtil.SqlError, function(){
-            return 'SqlError is expected'
-          })
+        var tracker = new MigrationTracker(db, {table: testTable})
+        return tracker.up('12345').then(function(){
+        }).then(function(){
+          return tracker.down('12345')
+        }).then(function(){
+          return testUtil.migrationIsMarkedComplete('12345', testTable)
+        }).then(function(){
+          throw 'migration should not be marked complete, but was'
+        }).catch(testUtil.SqlError, function(){
+          return 'SqlError is expected'
+        })
       })
     })
   })
