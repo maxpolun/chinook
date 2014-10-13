7635e620c7f3db2a12c081db80e8374f52d051ee
Max Polun <mpolun@mdsol.com>
2014-10-13 13:48:27 -0400

    add new e2e specs

--- e2e/migrateSpec.js
+++ e2e/migrateSpec.js
@@ -31,4 +31,26 @@ describe('migrations up', function(){
       })
     })
   })
+  describe('multiple sql migrations', function(){
+    pit('should run the sql', function(){
+      return Promise.using(testUtil.fixture(__dirname, 'multiple-sql-migrations'), function(dir){
+        return testUtil.runCmd(dir, ['up']).then(function(){
+          return Promise.all([
+            testUtil.expectTableToExist('users'), 
+            testUtil.expectTableToExist('thingies')
+          ])
+        })
+      })
+    })
+    pit('should update the migrations_complete table', function(){
+      return Promise.using(testUtil.fixture(__dirname, 'multiple-sql-migrations'), function(dir){
+        return testUtil.runCmd(dir, ['up']).then(function(){
+          return Promise.all([
+            testUtil.migrationIsMarkedComplete('001'),
+            testUtil.migrationIsMarkedComplete('002')
+          ])
+        })
+      })
+    })
+  })
 })
